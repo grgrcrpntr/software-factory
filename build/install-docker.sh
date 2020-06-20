c5131b2f36dcab95d7281f4f0e6c6ece662b9cb8
Grégoire Carpentier <43974188+grgrcrpntr@users.noreply.github.com>
2020-06-20 10:00:38 +0200

    Update install-docker.sh

--- build/install-docker.sh
+++ build/install-docker.sh
@@ -8,6 +8,14 @@ sudo add-apt-repository \
    stable"
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
+
+# start docker
 systemctl enable docker
 systemctl start docker
+
+# add users to docker group (will enable docker command w/o sudo)
+# more : https://www.youtube.com/watch?time_continue=56&v=XfZvKLNXC9M&feature=emb_logo
 usermod -aG docker ### a non-root user here ###
+
+# update permissions on /var/run/docker.sock (enable docker containers to communicate w/ the host docker engine)
+chmod 666 /var/run/docker.sock
